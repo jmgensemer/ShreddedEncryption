d2703d42cbddc2d2dfb39e6215fb882e6aff743c
jmgensemer <jmgensemer@mix.wvu.edu>
2016-04-21 16:52:09 -0400

    Added files via upload

--- GUIs.py
+++ GUIs.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilenames
 from Read_Files import *
+from PIL import ImageTk,Image
 import os
 import csv
 class Window(Frame):
@@ -8,12 +9,25 @@ class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
+        self.image = Image.open('/Applications/Shredded Encryption/Background.jpg')
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background_label = Label(self, image=self.background_image)
+        self.background_label2 = Label(self, image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=2, relheight=2)
+        self.background_label2.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.labelText = "  Welcome to\nShredded Encrpytion\n" \
+                         "   Version 1.0"
+        self.welcomeLabel = Label(self,text= self.labelText,)
+        self.welcomeLabel.place(x=0,y=0)
         self.init_window()
         self.init_EncryptedList()
         self.init_FButton()
         self.exitButton()
+        self.changeKeyButton()
     def init_window(self):
         self.master.title("Shredded Encryption")
+
         self.pack(fill=BOTH, expand=1)
 
     def init_EncryptedList(self):
@@ -22,44 +36,55 @@ class Window(Frame):
         for x in Encrypted_List:
             Lb.insert(i,x)
             i += 1
-        Lb.place(x=400, y=100)
+        Lb.place(x=150, y=0)
         DecryptButton = Button(self,text = "Decrypt Files", command = lambda: self.DecryptFiles(Lb.curselection()))
-        DecryptButton.place(x=420,y = 310)
+        DecryptButton.place(x=170,y = 210)
 
     def init_FButton(self):
-        fbutton = Button(self, text = "Encrypt a File", command = self.load_file)
-        fbutton.place(x=200,y=150)
+        fbutton = Button(self, text = "Encrypt Files", command = self.load_file)
+        fbutton.place(x=20,y=60)
+
+    def changeKeyButton(self):
+        cKeyButton = Button(self, text = "Change Key", command = self.changeKeyCommand)
+        cKeyButton.place(x=22,y=93)
 
     def load_file(self):
         fname = askopenfilenames()
         size = len(fname)
         for x in range(size):
-            piece = splitFiles(fname[x])
+            piece = splitFiles(fname[x],key)
             Encrypted_List.append((fname[x].rsplit('/', 1)[1]))
             File_List.append((fname[x],piece))
         self.init_EncryptedList()
 
+
     def exitButton(self):
-        exit = Button(self,text = 'quit', command = self.quitCommand)
-        exit.place(x=300,y=350)
+        exit = Button(self,text = 'Quit', command = self.quitCommand)
+        exit.place(x=45,y=130)
     def quitCommand(self):
-        with open('/Applications/Shredded Encryption/directory.csv', 'w') as csvfile:
-            fieldnames = ['Filename', 'First Piece']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for x in File_List:
-                writer.writerow({'Filename': x[0], 'First Piece': x[1]})
-            self.quit()
+            xpressed()
+
+    def changeKeyCommand(self):
+        global key
+        global File_List
+        a = []
+        newKey = getKey()
+        for x in File_List:
+            piece_Files(x,key)
+            piece = splitFiles(x[0],newKey)
+            a.append((x[0],piece))
+        key = newKey
+        File_List = a
 
 
     def DecryptFiles(self,x):
         for y in range(0,len(x)):
             if len(x) > 1:
-                piece_Files(File_List[x[y-y]])
+                piece_Files(File_List[x[y-y]],key)
                 del Encrypted_List[x[y-y]]
                 del File_List[x[y-y]]
             else:
-                piece_Files(File_List[x[y]])
+                piece_Files(File_List[x[y]],key)
                 del Encrypted_List[x[y]]
                 del File_List[x[y]]
 
@@ -68,15 +93,33 @@ class Window(Frame):
 
 Encrypted_List = []
 File_List = []
+key = getKey()
 if os.path.exists('/Applications/Shredded Encryption/directory.csv'):
-    with open('/Applications/Shredded Encryption/directory.csv') as f:
+    with open('/Applications/Shredded Encryption/directory.csv', 'r') as f:
             next(f)
             File_List= [tuple(line) for line in csv.reader(f)]
+
+if os.path.exists('/Applications/Shredded Encryption/fjkldfemsf'):
+    with open('/Applications/Shredded Encryption/fjkldfemsf', 'rb') as f:
+            key = f.readline()
 
 for x in File_List:
     Encrypted_List.append(x[0].rsplit('/', 1)[1])
 
+def xpressed():
+    with open('/Applications/Shredded Encryption/directory.csv', 'w') as csvfile:
+        fieldnames = ['Filename', 'First Piece']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for x in File_List:
+            writer.writerow({'Filename': x[0], 'First Piece': x[1]})
+    os.remove('/Applications/Shredded Encryption/fjkldfemsf')
+    with open('/Applications/Shredded Encryption/fjkldfemsf', 'wb') as f:
+            f.write(key)
+    root.destroy()
+
 root = Tk()
-root.geometry("600x400")
+root.geometry("300x275")
+root.protocol('WM_DELETE_WINDOW', xpressed)
 app = Window(root)
 root.mainloop()
--- Read_Files.py
+++ Read_Files.py
@@ -2,6 +2,12 @@ import codecs
 import random
 import string
 import os
+from Crypto import Random
+from Crypto.Cipher import AES
+import hashlib
+import time
+
+
 
 """Generates a 10 character long string
     Args:
@@ -25,13 +31,18 @@ def filenames(nameamounts):
         x.append(filename_generator())
     return x
 
+def change_filename(filename):
+    return ''.join((filename[1], filename[0], filename[9], filename[4], filename[3], filename[8], filename[7], filename[6], filename[5], filename[2]))
+
+
 """Fragments File passed
     Args:
         readFile: File that will be Fragmented
     Returns:
         Name of the first piece
 """
-def splitFiles(readFile):
+def splitFiles(readFile,key):
+    encrypt_file(readFile, key)
     readf = open(readFile,"rb")
     filesize = os.path.getsize(readFile)
     pieceSize = findSizeofPieces(filesize)
@@ -42,14 +53,14 @@ def splitFiles(readFile):
     for i in range(splitNumber):
         if (i == splitNumber - 1):
             piece = open("/Applications/Shredded Encryption/" + fnames[i], "w")
-            piece.write("haltcode00")
+            piece.write(change_filename("haltcode00"))
             piece.close()
             piece = open("/Applications/Shredded Encryption/" + fnames[i], "ab")
             piece.seek(10)
             piece.write(readf.read(lastPiece))
         else:
             piece = open("/Applications/Shredded Encryption/" + fnames[i], "w")
-            piece.write(fnames[i + 1])
+            piece.write(change_filename(fnames[i + 1]))
             piece.close()
             piece = open("/Applications/Shredded Encryption/" + fnames[i], "ab")
             piece.seek(10)
@@ -57,7 +68,7 @@ def splitFiles(readFile):
         piece.close()
     readf.close()
     os.remove(readFile)
-    return fnames[0]
+    return change_filename(fnames[0])
 
 """Generates the size of each File Piece
     Args:
@@ -68,32 +79,32 @@ def splitFiles(readFile):
 def findSizeofPieces(fileSize):
     x = 0
     if (fileSize <= 100):
-        x = 10
+        x = 5
     elif (fileSize <= 1000):
-        x = 50
+        x = 25
     elif (fileSize <= 10000):
-        x = 1000
+        x = 500
     elif (fileSize <= 100000):
-        x = 10000
+        x = 5000
     elif (fileSize <= 1000000):
-        x = 100000
+        x = 50000
     elif (fileSize <= 50000000):
-        x = 1000000
+        x = 500000
     elif (fileSize <= 100000000):
-        x = 10000000
+        x = 5000000
     elif (fileSize <= 1000000000):
-        x = 100000000
+        x = 50000000
     else:
-        x = 1000000000
+        x = 500000000
     return x
 
 """Combinds File Pieces to recreate Original File
     Args:
         file_tuple: A tuple that contains (Path/Filename, First Split Piece)
 """
-def piece_Files(file_tuple):
+def piece_Files(file_tuple,key):
     wholefile = open(file_tuple[0], "wb")
-    name = file_tuple[1]
+    name = change_filename(file_tuple[1])
     while(name != "haltcode00"):
         open_piece = codecs.open("/Applications/Shredded Encryption/"+name,"r", encoding="utf-8")
         nextname = open_piece.read(len(name))
@@ -103,5 +114,48 @@ def piece_Files(file_tuple):
         size = os.path.getsize("/Applications/Shredded Encryption/" + name) - len(name)
         wholefile.write(open_piece.read(size))
         os.remove("/Applications/Shredded Encryption/" + name)
-        name = nextname
+        name = change_filename(nextname)
     wholefile.close()
+    decrypt_file(file_tuple[0], key)
+
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+def encrypt_text(plaintext, key):
+    plaintext = pad(plaintext)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(plaintext)
+
+def decrypt_text(cipheredtext, key):
+    iv = cipheredtext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(cipheredtext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
+
+def encrypt_file(file_name, key):
+    fileout_name = file_name + '.enc'
+    with open(file_name, 'rb') as fo:
+        plaintext = fo.read()
+    enc = encrypt_text(plaintext, key)
+    with open(fileout_name, 'wb') as fo:
+        fo.write(enc)
+        os.remove(file_name)
+        os.rename(fileout_name,file_name)
+
+def decrypt_file(file_name,key):
+    fileout_name = file_name + '.dec'
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt_text(ciphertext, key)
+    with open(fileout_name, 'wb') as fo:
+        fo.write(dec)
+    os.remove(file_name)
+    os.rename(fileout_name, file_name)
+
+def getKey():
+    password = bytes(memoryview(time.ctime().encode()))
+    dk = hashlib.pbkdf2_hmac('sha256', password, b'salt', 100000)
+    return dk
+
+
