a4309872f0945752a049fda58ee997bb7489ece8
FahadAlhajjaj <fhalhajjaj@mix.wvu.edu>
2016-04-26 16:17:56 -0400

    Added files via upload

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -1,5 +1,10 @@
 import os
 
+"""calls an OpenFace scrypt called classifier to facial recognaize an image.
+    Returns:
+        return 0 if user is not recognize, or there is no photo in folder
+        return 1 if user is identified with "+80%" accuracy.
+"""
 def facialRecognition():
     path1 = "/Applications/ShreddedEncryption/appImages/pic.jpg"
     path2 = "/Users/fedo/Desktop/LastTry/openface/"
